35850cc093dabbf21cd92f9d77b500fc4bb8cc2d
Kai Schmidt <kaikaliischmidt@gmail.com>
2023-10-10 20:12:50 -0700

    make it an array instead of an object

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
-use std::collections::BTreeMap;
-
 use serde::*;
 use uiua::primitive::Primitive;
 
 fn main() {
-    let mut map = BTreeMap::new();
+    let mut prims = Vec::new();
     for prim in Primitive::non_deprecated() {
         if let Some(names) = prim.names() {
-            map.insert(
-                names.text,
-                PrimData {
-                    glyph: names.glyph,
-                    description: prim
-                        .doc()
-                        .map(|doc| doc.short_text())
-                        .unwrap_or_default()
-                        .into(),
-                },
-            );
+            prims.push(PrimData {
+                name: names.text.into(),
+                glyph: names.glyph,
+                description: prim
+                    .doc()
+                    .map(|doc| doc.short_text())
+                    .unwrap_or_default()
+                    .into(),
+            });
         }
     }
-    let json = serde_json::to_string_pretty(&map).unwrap();
+    let json = serde_json::to_string_pretty(&prims).unwrap();
     std::fs::write("uiua_functions.json", json).unwrap();
 }
 
 #[derive(Serialize)]
 struct PrimData {
+    name: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     glyph: Option<char>,
     description: String,
